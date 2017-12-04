fb69ab3c6db2a205fac2b3135e6d93e3a5d23c4b
Vladyslav Kolinko <KolinkoVlad@gmail.com>
2017-12-04 19:28:38 +0200

    first commit

---
+++ this.py
@@ -0,0 +1,46 @@
+##x = 15
+####print("x = {1}, x+5 = {0} ".format (x, x+5))
+##
+##print("x = {0:d}, binary : x={1:b} , hexagonal : x={2:x}" .format (x,x,x))
+####print (" {2}, {1}, {0} , {2}".format({a},{b},{c}))
+##a = int(input("Enter number ..."))
+##if a < -5:
+##    print("Low")
+##elif -5 <= a <= 5:
+##    print("Mid")
+##else:
+##    print("Hight")
+##
+##a= int(input("Enter age"))
+##
+##if a>16:
+##    print("Hi")
+##elif a > 16 and < 30:
+##    print("Hello")
+##else a<30:
+##    print("Good day")
+##s = "sdfsdfsf"
+##print( s[:-3])
+##a= 10
+##i=0
+##
+##while(i<a):
+##        
+##       print(i)
+##        i = i+1
+##
+##
+##a=1
+##n=10
+##while(a<=n):
+##        print(a)
+##        a = a+1
+
+a= int(input("Enter number"))
+
+while(a>0):
+        print(a)
+        a = a-1
+                
+
+        

bd7a5ff2029e979d7801c1fd4e7516e11bd53841
rostik.d <daniliv.rostik624@gmail.com>
2017-12-05 20:46:53 +0200

    SecondHWTypeARG

---
+++ TypeArg.py
@@ -0,0 +1,21 @@
+def foo(*a):
+    z = len(a)
+    if (z > 1):
+        print("Кількість аргументів не може бути більше 1.\n format foo(argv1)" )
+    b = a
+    c = a
+    v = b[0] + c[0] 
+#    vv = len(v)
+#    pg = b[0]
+    if type(v) is str:
+        print("Змінна: str")
+    elif type(v) is int:
+        print("Змінна: int")
+    elif type(v) is float:
+        print("Змінна: float")
+    else: 
+        print("Змінна: НЕВІДОМА!")
+#    for v in range(0, vv):
+#        print(pg[0])
+
+print(foo("powefkwpoefk"))
